09eda12b21a92658bb36905c0f51863d925466e3
Kai Nguyen <nguyenchicuong2402@gmail.com>
2025-02-05 16:44:34 +0700

    update entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,5 +4,3 @@ echo "🔄 Fetching secrets from Infisical..."
 export $(infisical export --domain "$VAULT_URL" --projectId "$VAULT_PROJECT_ID" --env "$VAULT_ENV" --format=dotenv | xargs)
 
 echo "✅ Secrets loaded."
-
-echo "🚀 Starting Application..."
